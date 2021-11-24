a1879f7a1fffa62132249d08f7b42a18a89b820f
Mazzucca Kristian <mazzuccakristian@gmail.com>
2021-11-24 09:54:34 +0100

    Getters and setters added.

--- Student.cpp
+++ Student.cpp
@@ -1,12 +1,76 @@
 #include "Student.h"
 
 Student::Student(int roll, string name, string lastName, int engMark, int mathMark, int sciMark, int secLangMark, int csMark){
+    SetRoll(roll);
+    SetName(name);
+    SetLastName(lastName);
+    setEnglishMark(engMark);
+    SetMathMark(mathMark);
+    SetScienceMark(sciMark);
+    SetSecondLanguageMark(secLangMark);
+    SetComputerScienceMark(csMark);
+}
+
+void Student::SetRoll(int roll){
     Roll = roll;
+}
+
+void Student::SetName(string name){
     Name = name;
+}
+
+void Student::SetLastName(string lastName){
     LastName = lastName;
+}
+
+void Student::setEnglishMark(int engMark){
     English = engMark;
+}
+
+void Student::SetMathMark(int mathMark){
     Math = mathMark;
+}
+
+void Student::SetScienceMark(int sciMark){
     Science = sciMark;
+}
+
+void Student::SetSecondLanguageMark(int secLangMark){
     SecondLanguage = secLangMark;
+}
+
+void Student::SetComputerScienceMark(int csMark){
     ComputerScience = csMark;
+}
+
+int Student::GetRoll(){
+    return Roll;
+}
+
+string Student::GetName(){
+    return Name;
+}
+
+string Student::GetLastName(){
+    return LastName;
+}
+
+int Student::GetEnglishMark(){
+    return English;
+}
+
+int Student::GetMathMark(){
+    return Math;
+}
+
+int Student::GetScienceMark(){
+    return Science;
+}
+
+int Student::GetSecondLanguageMark(){
+    return SecondLanguage;
+}
+
+int Student::GetComputerScienceMark(){
+    return ComputerScience;
 }
--- Student.h
+++ Student.h
@@ -6,11 +6,29 @@ using std::cout;
 using std::endl;
 
 class Student{
-    public:
+    private:
         int Roll;
         string Name, LastName;
         int English, Math, Science, SecondLanguage, ComputerScience;
 
     public:
         Student(int roll, string name, string lastName, int engMark, int mathMark, int sciMark, int secLangMark, int csMark);
+        //Setters
+        void SetRoll(int roll);
+        void SetName(string name);
+        void SetLastName(string lastName);
+        void setEnglishMark(int engMark);
+        void SetMathMark(int mathMark);
+        void SetScienceMark(int sciMark);
+        void SetSecondLanguageMark(int secLangMark);
+        void SetComputerScienceMark(int csMark);
+        //Getters
+        int GetRoll();
+        string GetName();
+        string GetLastName();
+        int GetEnglishMark();
+        int GetMathMark();
+        int GetScienceMark();
+        int GetSecondLanguageMark();
+        int GetComputerScienceMark();
 };
